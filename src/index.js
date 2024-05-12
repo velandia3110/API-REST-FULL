3d2c0656bd1816d360ecfa5c7a69a2c19ca2e837
Julian Velandia <julianvelandia3110@gmail.com>
2024-05-12 18:26:55 -0500

    port changed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import app from './app.js';
 import './database';
 
-app.listen(3000);
-console.log('Server listen on port ' + 3000);
+app.listen(10000);
+console.log('Server listen on port ' + 10000);
