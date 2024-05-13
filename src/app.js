fb72026a40aa33a47e29adee0042ea9ee0e537b1
Julian Velandia <julianvelandia3110@gmail.com>
2024-05-12 20:50:14 -0500

    cors modified again

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
 
 app.all('*',function (req,res,next){
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 })
 
