801f1465bfbc0b0634666797b9f940965861eb45
Julian Velandia <julianvelandia3110@gmail.com>
2024-05-12 19:05:08 -0500

    directories filled again

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 const router = Router();
 
 import * as authController from '../controllers/auth.controller.js';
-import {authjwt,verifySignup} from '../middlewares';
+import {authjwt,verifySignup} from '../middlewares/index.js';
 
 router.post('/signin',authController.signin);
 router.post('/signup/',[authjwt.verifyToken,authjwt.isAdmin,verifySignup.checkRolesExisted,verifySignup.checkDuplicatedUsernameOrEmail],authController.signup);
